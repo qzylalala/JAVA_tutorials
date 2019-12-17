0bb2110a19c753d2b10fc61534f89532a8bb44f2
qzylalala <304228244@qq.com>
2019-12-17 16:26:39 +0800

    example

---
+++ test/HelloWorld.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = "hello";
+        //String s2 = "HELLO".toLowerCase();
+        System.out.println(s1);
+        System.out.println(s2);
+        if (s1 == s2) {
+            System.out.println("s1 == s2");
+        } else {
+            System.out.println("s1 != s2");
+        }
+
+        if (s1.equals(s2)) {
+            System.out.println("s1 equals s2");
+        } else {
+            System.out.println("s1 not equals s2");
+        }
+    }
+}
